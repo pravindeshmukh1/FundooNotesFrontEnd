72d712e607ad4d12a437bd98bc2a9bd8287266fc
pravindeshmukh1 <pravin1desh@gmail.com>
2020-06-28 11:00:01 +0530

    ADDED: pin and unpin note

--- src/app/components/notedialog/notedialog.component.ts
+++ src/app/components/notedialog/notedialog.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NoteService } from 'src/app/services/note.service/note.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-notedialog',
@@ -9,17 +11,56 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class NotedialogComponent implements OnInit {
   title: string;
   description: string;
+  isPined: boolean;
 
   constructor(
     public dialogRef: MatDialogRef<NotedialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private noteService: NoteService,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
     this.title = this.data.note.title;
     this.description = this.data.note.description;
+    this.isPined = this.data.note.pined;
   }
-
+  pinNote() {
+    let noteData = {
+      noteIdList: this.data.note.id,
+      isPined: true,
+    };
+    this.noteService.pinNote(noteData).subscribe(
+      (res) => {
+        this.snackBar.open('Pined note', '', {
+          duration: 2000,
+        });
+      },
+      (err) => {
+        this.snackBar.open('Error occured Pin note', '', {
+          duration: 2000,
+        });
+      }
+    );
+  }
+  unPinNote() {
+    let noteData = {
+      noteIdList: this.data.note.id,
+      isPined: false,
+    };
+    this.noteService.unPinNote(noteData).subscribe(
+      (res) => {
+        this.snackBar.open('UnPined note', '', {
+          duration: 2000,
+        });
+      },
+      (err) => {
+        this.snackBar.open('Error occured UnPin note', '', {
+          duration: 2000,
+        });
+      }
+    );
+  }
   close() {
     this.dialogRef.close({
       noteIdList: this.data.note.id,
--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -9,6 +9,9 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class NotesComponent implements OnInit {
   notes: Array<any>;
+  pined: Array<any>;
+  notesLength: number;
+  pinedLength: number;
 
   constructor(
     private noteService: NoteService,
@@ -17,19 +20,46 @@ export class NotesComponent implements OnInit {
 
   ngOnInit() {
     this.getNotes();
+    this.getPinnedNotes();
   }
 
   getNotes() {
     this.noteService.getNote().subscribe(
       (res: any) => {
         let notes = res.data.data.filter((element: any) => {
-          return element.isArchived === false && element.isDeleted === false;
+          return (
+            element.isArchived === false &&
+            element.isDeleted === false &&
+            element.isPined === false
+          );
         });
         this.notes = notes;
         this.notes.reverse();
+        this.notesLength = res.length;
       },
       (err) => {
         this.snackBar.open('Error occured at get note', '', {
+          duration: 2000,
+        });
+      }
+    );
+  }
+  getPinnedNotes() {
+    this.noteService.getNote().subscribe(
+      (res: any) => {
+        let notes = res.data.data.filter((element: any) => {
+          return (
+            element.isPined === true &&
+            element.isArchived === false &&
+            element.isDeleted === false
+          );
+        });
+        this.pined = notes;
+        this.pined.reverse();
+        this.pinedLength = res.length;
+      },
+      (err) => {
+        this.snackBar.open('Error occured at get pin note', '', {
           duration: 2000,
         });
       }
